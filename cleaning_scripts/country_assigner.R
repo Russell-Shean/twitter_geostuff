05c07aff1d556429d476d89ec20f5ade3ca697e8
Russell-Shean <russshean@gmail.com>
2021-11-12 17:45:12 +0800

    point buffer not shape buffer

--- cleaning_scripts/country_assigner.R
+++ cleaning_scripts/country_assigner.R
@@ -1,5 +1,7 @@
 # https://tapiquen-sig.jimdofree.com/english-version/free-downloads/world/
 
+require(sf)
+
 world_shp2 <- st_read("./shape_files/World_Countries.shp")
 
 
@@ -19,3 +21,13 @@ for(i in 1:nrow(world_shp2)){
 
 point_counts_us <- st_intersects(world_shp2[230,], tweet_points_sf)
 
+
+buffered_us <- st_buffer(world_shp2[230,],dist = 0.5)
+
+buffered_points <- st_buffer(tweet_points_sf, dist = 10)
+
+
+library(tmap)
+tmap_mode("view")
+
+tm_shape(buffered_us)+tm_borders(col= "red")+tm_shape(world_shp2[230,])+tm_borders(col = "green")+tm_shape(tweet_points_sf)+tm_dots(col = "blue")
