5870e263d8f229ddd76350cb65a0e83004ba9b90
Russell-Shean <russshean@gmail.com>
2021-12-07 17:29:54 +0800

    replace with world window?

--- world_unions.R
+++ world_unions.R
@@ -1,4 +1,4 @@
-world_union <- st_union(world_tweets)
+#world_union <- st_union(world_tweets)
 
 library(geojsonsf)
 
@@ -19,3 +19,38 @@ wurld_3395 %>% st_is_longlat()
 
 
 wurld_3395 %>% st_union() %>% tm_shape() + tm_borders()
+
+
+
+#kde
+tm_shape(wurld)+tm_borders()+tm_shape(tweet_points_sf)+tm_dots(col = "green")
+
+choose_bw<- function(spdf){
+  X <- coordinates(spdf)
+  stigma <- c(sd(X[,1]),sd(X[,2])) *(2/(3*nrow(X)))^(1/6)
+  return(stigma)
+}
+
+
+library(tmaptools)
+tmap_mode("view")
+tweet_dens <- smooth_map(tweet_points_sp, cover=wurld_sp)
+
+
+#this sources a file the builds a depreciated function from tmap
+
+#discussion: https://github.com/r-tmap/tmap/issues/488
+
+
+#this loads smooth_map()
+#source:
+source("https://raw.githubusercontent.com/mtennekes/oldtmaptools/master/R/smooth_map.R")
+
+#this loads smooth_raster_cover()
+source("https://raw.githubusercontent.com/mtennekes/oldtmaptools/master/R/smooth_raster_cover.R")
+
+# the shape files need to be sp instead of sf
+tweet_points_sp <- as(tweet_points_sf,"Spatial")
+
+wurld_sp <- as(wurld, "Spatial")
+
